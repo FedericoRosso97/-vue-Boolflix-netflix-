6ef057e1477f9d0ddb3804d8791d4bc14f229932
FedericoRosso97 <federicorosso97@gmail.com>
2023-06-21 11:57:04 +0200

    link components e check funzionamento

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,21 @@
 <script >
-    export default{
-        
+    export default {
+    name: 'AppHeader',
+    data(){
+      return {
+
+      }
+    },
+    components: {
+     
     }
+}
    </script>
    
    <template>
-     
+     <div>
+        <h1>Header</h1>
+     </div>
    </template>
    
    <style scoped>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,19 @@
 <script >
- export default{
-    
- }
+ export default {
+    name: 'AppMain',
+    data(){
+      return {
+
+      }
+    },
+    components: {
+}}
 </script>
 
 <template>
-  
+  <div>
+    <h1>Main</h1>
+  </div>
 </template>
 
 <style scoped>
