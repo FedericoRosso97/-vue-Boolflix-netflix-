adf556a727975397ae6987cbda30ee0ad86683c0
FedericoRosso97 <federicorosso97@gmail.com>
2023-06-21 15:27:51 +0200

    faccio richiesta axios

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,32 @@
 <script >
+import axios from 'axios';
  export default {
     name: 'AppMain',
     data(){
       return {
-
+   
       }
     },
     components: {
-}}
+
+    },
+    created(){
+        axios.get("https://api.themoviedb.org/3/search/movie?api_key=05e361d095d6a9d0db65612c54a8d5f5&query=ritorno+al+futuro")
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  })
+  
+    }
+
+}
 </script>
 
 <template>
