5a85b01d77623a5a6b3594bb38cf18f76726d15a
FedericoRosso97 <federicorosso97@gmail.com>
2023-06-26 16:43:38 +0200

    converto lingua orginiale in img

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -10,10 +10,12 @@ import cardFilm from './cardFilm.vue'
           apiLink:'https://api.themoviedb.org/3/search/movie?api_key=05e361d095d6a9d0db65612c54a8d5f5&query=back&include_adult=false&language=en-US&page=1'
       }
     },
+    
     components: {
        searchBar,
        cardFilm,
     },
+    
     methods:{
         getMovies(search){
         axios.get(this.apiLink,{
@@ -21,26 +23,23 @@ import cardFilm from './cardFilm.vue'
             query:search,
           }
         })
-      .then( (response) => {
-    // handle success
+       .then( (response) => {
         console.log(response);
         console.log(response.data.results)
         this.filmList=response.data.results
         console.log(this.filmList)
         })
         .catch(function (error) {
-    // handle error
-        console.log(error);
+         console.log(error);
          })
-       
     },
-
+     
+     //fine methods
     },
+    
     create(){
         this.getMovies
     }
-
-   
 }
 </script>
 
@@ -51,26 +50,16 @@ import cardFilm from './cardFilm.vue'
     </header>
   <main>
     <div class="container">
-        
-     <div class="row">
-        <div class="col-3 d-felx p-2 myCard">
+        <div class="row wrap-nowrap">
             <cardFilm v-for="films in filmList"
             :filmElement="films"
             />
-            
-         <!-- <p>Titolo:{{ filmList[index].title }}</p>
-          <p>Titolo originale:{{ filmList[index].original_title }}</p>
-          <p>Lingua originale:{{ filmList[index].original_language}}</p>
-          <p>Voto:{{ filmList[index].vote_average }}</p>-->
         </div>
-     </div>
     </div>
-    
+            
   </main>
 </template>
 
 <style scoped>
-  .myCard{
-    border:1px solid black;
-  }
+ 
 </style>
--- src/components/cardFilm.vue
+++ src/components/cardFilm.vue
@@ -6,25 +6,44 @@
     },
     data(){
       return {
-         
+         flags:['en.jpg','fr.jpg','it.png',],
       }
     },
     methods:{
-        
+      isFlagPresent(language){
+         if(this.flags.includes(language+'.jpg')){
+            console.log('true')
+          return true
+         }
+         console.log('false')
+         return false;
+     },
+
+     getImagePath(imgSource){
+      return new URL(`../assets/img${imgSource}.jpg`, import.meta.url);
+     }
     }
     
 }
    </script>
    
    <template>
-        <div class="col-3 d-felx p-2 "> 
+         <div class="col-3 p-2 myCard "> 
             <p>Titolo:{{ filmElement.title }}</p>
-          <p>Titolo originale:{{ filmElement.original_title }}</p>
-          <p>Lingua originale:{{ filmElement.original_language}}</p>
-          <p>Voto:{{ filmElement.vote_average }}</p>
-        </div>
+            <div>
+               <img v-if="isFlagPresent(filmElement.original_language)" 
+               :src="getImagePath(filmElement.original_language)" alt="img">
+               <span v-else>
+                  <p>Lingua originale:{{ filmElement.original_language}}</p>
+               </span>
+            </div>
+            <p>Titolo originale:{{ filmElement.original_title }}</p>
+            <p>Voto:{{ filmElement.vote_average }}</p>
+         </div>
    </template>
    
    <style scoped>
-   
+    .myCard{
+    border:1px solid black;
+  }
    </style>
